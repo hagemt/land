38fb4fa9bbbea060378e08de8948b24be785b649
Tor E Hagemann <hagemt@users.noreply.github.com>
2022-04-30 16:28:41 -0700

    chore: make it work

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+
+const app = new Application()
+
+app.use(({ response }) => {
+  response.body = {
+    dino: Deno.env.get('DINO') || 'Hello, World!',
+  }
+})
+
+await app.listen({
+  port: Number(Deno.env.get('PORT') || '8080'),
+})
